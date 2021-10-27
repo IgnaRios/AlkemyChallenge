feeb6211d0b1e5b94599bc92a5704c5a18f873f2
IgnaRios <ignacio.rios@hotmail.es>
2021-10-27 11:12:27 -0300

    creacion del server

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json);
+
+const PORT = process.env.PORT ? process.env.PORT : 3000;
+
+
+
+
+
+
+const server = app.listen(PORT, () => {
+    console.log('Escuchando en el puerto', PORT)
+})
